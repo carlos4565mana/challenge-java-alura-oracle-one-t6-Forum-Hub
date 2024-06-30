6f595b7d1b50d96445de83b8ffa2f6bc3646da73
carlos4565mana <carloscal61@gmail.com>
2024-06-30 13:18:25 -0300

    first commit

---
+++ src/main/java/br/com/challenge_alura_one_t6/AluraForum/AluraForumApplication.java
@@ -0,0 +1,13 @@
+package br.com.challenge_alura_one_t6.AluraForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/challenge_alura_one_t6/AluraForum/AluraForumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.challenge_alura_one_t6.AluraForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
